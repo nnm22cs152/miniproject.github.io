43efe6aaa6652f6013b591b8f45c53fd5993e83f
nnm22cs152 <141390673+nnm22cs152@users.noreply.github.com>
2023-10-06 11:14:30 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".slider-image");
+    let currentIndex = 0;
+  
+    function showImage(index) {
+      images.forEach((image, i) => {
+        if (i === index) {
+          image.style.display = "block";
+        } else {
+          image.style.display = "none";
+        }
+      });
+    }
+  
+    function nextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      showImage(currentIndex);
+    }
+  
+    showImage(currentIndex);
+    setInterval(nextImage, 3000);
+  });
+  
